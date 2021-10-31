032ecf3ec3eda5af5f9f0e98a5abbac824a25039
vondeF <fednov27@gmail.com>
2021-10-31 12:14:20 +0300

    Сервер возвращает текущее время

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/api/simpleNumber/', methods=['GET'])
-def getSimpleNumber():
-    return '15'
+@app.route('/', methods=['GET'])
+def getCurrentTime():
+    return str(datetime.now().time())
 
 if __name__ == '__main__':
     app.run(debug=True)
