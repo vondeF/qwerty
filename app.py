5c410ecdd4513750b45863dede00f142045561f1
vondeF <fednov27@gmail.com>
2021-11-02 21:31:07 +0300

    Сделал комментарии

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 from random import randint
 
-app = Flask(__name__)
+app = Flask(__name__)  # создаем экземпляр класса, передаем дерективу __name__
+# в случае импорта __name__ содержит имя текужего файла
+# в случае самостоятельного запуска __name__ = __main__
 
-@app.route('/', methods=['GET'])
-def hello():
-    return 'Тут нет ничего интересного. Отправляйся по другим URL'
 
-@app.route('/test/api/time', methods=['GET'])
+@app.route('/', methods=['GET'])  # создаем декоратор; указываем, по какому URL будет отрабатывать функция
+def index():
+    return render_template('index.html')  # рендерим отдельный HTML-файл, чтоб не грузить основной файл
+
+
+@app.route('/currentTime', methods=['GET'])
 def getCurrentTime():
     return str(datetime.now().time())
 
-@app.route('/test/api/randomNumber', methods=['GET'])
+
+@app.route('/randomNumber', methods=['GET'])
 def getRandomNumber():
     return str(randint(0, 10))
 
-@app.route('/test/api/certainNumber/<a>', methods=['GET'])
-def getCertainNumber(a):
-    return str(a)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # запуск локального веб-сервера; debug=True - видим все ошибки на самом сервере
--- client.py
+++ client.py
@@ -1,4 +1,10 @@
 import requests
 
-response = requests.get('http://127.0.0.1:5000/test/api/time')
-print(response.content)
+response = requests.get('http://127.0.0.1:5000/currentTime')  # get-запрос по соответсвующему адресу
+
+if response.status_code == 200:
+    print('Данные найдены')
+elif response.status_code == 404:
+    print('Данные не найдены')
+
+print('По запросу получили: ' + response.text)
