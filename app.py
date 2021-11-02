b769a63d5fb4b03fbd30a250355d61e6a707e80a
vondeF <fednov27@gmail.com>
2021-11-02 18:34:32 +0300

    Сделал клиент, который обращается к серверу за текущим временем

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from datetime import datetime
+from random import randint
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
+def hello():
+    return 'Тут нет ничего интересного. Отправляйся по другим URL'
+
+@app.route('/test/api/time', methods=['GET'])
 def getCurrentTime():
     return str(datetime.now().time())
+
+@app.route('/test/api/randomNumber', methods=['GET'])
+def getRandomNumber():
+    return str(randint(0, 10))
+
+@app.route('/test/api/certainNumber/<a>', methods=['GET'])
+def getCertainNumber(a):
+    return str(a)
 
 if __name__ == '__main__':
     app.run(debug=True)
